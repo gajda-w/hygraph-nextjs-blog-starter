98d4ee38e528c0f04c3dc17dfa55522ea2227e85
LSzpilowski <lszpilowski@gmail.com>
2024-02-23 09:47:46 +0000

    navbar-template-to-fix-width-and-darkmode

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import { Feather } from "lucide-react";
+import { ModeToggle } from "@/components/ModeToggle";
+
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export type Item = {
+  label: string;
+  url: string;
+  id: string;
+};
+
+export const Header = ({ items }: { items: Item[] }) => {
+  return (
+    <NavigationMenu className="max-w-full">
+      <NavigationMenuList className="flex min-w-96 justify-between">
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Feather />
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          {items.map((item) => (
+            <Link href={item.url} legacyBehavior passHref key={item.id}>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {item.label}
+              </NavigationMenuLink>
+            </Link>
+          ))}
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <ModeToggle />
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
