909c1f7347ac7c4bb9e028c8bca300d86e3c6ba4
LSzpilowski <lszpilowski@gmail.com>
2024-03-04 16:44:46 +0000

    adjust-footer-to-smaller-devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,27 +3,32 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="fixed inset-x-0 bottom-0 m-4 rounded-lg bg-white shadow dark:bg-gray-800">
-      <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+      <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between p-2 md:p-4">
+        <span className="text-center text-xs text-gray-500 dark:text-gray-400 md:text-sm">
           © 2024{" "}
-          <a href="https://github.com/gajda-w" className="hover:text-black hover:underline">
+          <a
+            href="https://github.com/gajda-w"
+            target="_blank"
+            rel="noopener noreferrer" // Added for security reasons
+            className="hover:text-black hover:underline"
+          >
             Gajda™
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center text-xs font-medium text-gray-500 dark:text-gray-400 md:text-sm">
           <li>
-            <a href="#" className="me-4 hover:text-black hover:underline  md:me-6">
+            <a href="#" className="mr-2 hover:text-black hover:underline md:mr-4">
               About
             </a>
           </li>
           <li>
-            <a href="#" className="me-4 hover:text-black hover:underline md:me-6">
+            <a href="#" className="mr-2 hover:text-black hover:underline md:mr-4">
               Privacy Policy
             </a>
           </li>
           <li>
-            <a href="#" className="me-4 hover:text-black hover:underline md:me-6">
+            <a href="#" className="mr-2 hover:text-black hover:underline md:mr-4">
               Licensing
             </a>
           </li>
