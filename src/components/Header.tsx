07e041e8610d70af34f7b9572230f2f339cc21d3
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-06-14 15:59:55 +0200

    refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-// import Link from "next/link";
-// import { Feather } from "lucide-react";
+import Link from "next/link";
+import { Feather } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
-// import {
-//   NavigationMenu,
-//   NavigationMenuList,
-//   NavigationMenuItem,
-//   navigationMenuTriggerStyle,
-// } from "@/components/ui/navigation-menu";
+
 import { HamburgerMenu } from "@/components/hamburger-menu";
-// import { NavigationLink } from "@/components/navigation-link";
+
 import { NavigationDocument } from "@/gql/graphql";
 import { executeGraphql } from "@/lib/graphql";
 
@@ -19,32 +14,28 @@ export const Header = async () => {
     variables: { navId: "main-navigation" },
   });
 
-  console.log(navigationData.navigation);
-
-  // TODO FIX THIS
-
   return (
-    <div className="sticky inset-x-0 top-0 z-10 bg-white py-3 shadow dark:bg-gray-800">
-      <div className="container">
-        <div className="grid w-full grid-flow-col grid-cols-[repeat(3,1fr)] items-center justify-items-center gap-4">
-          {/* <Link href="/">
-            <Feather
-              className={`h-9 cursor-pointer px-2.5 py-2 md:h-10 ${navigationMenuTriggerStyle()}`}
-            />
-          </Link> */}
-          <HamburgerMenu navigations={navigationData?.navigation?.link} />
-          {/* <NavigationMenu className="hidden max-w-full md:flex">
-            <NavigationMenuList className="flex justify-between">
-              {navigationData?.navigation?.link.map((link) => (
-                <NavigationMenuItem key={link.slug}>
-                  <NavigationLink navigationLink={link} className="mx-2 px-4 py-2 text-sm" />
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </NavigationMenu> */}
-          <ModeToggle />
+    <div className="sticky z-50 mx-4">
+      <header className="top-0 isolate z-10 py-4 md:sticky">
+        <div className="relative mx-auto max-w-7xl">
+          <div className="flex items-center justify-between">
+            <Link href="/">
+              <Feather />
+            </Link>
+            <div className="hidden gap-8 md:flex">
+              <div className="flex items-center gap-4">
+                {navigationData.navigation?.link.map((link) => (
+                  <Link key={link.slug} href={link.slug || ""}>
+                    <div>{link.displayText}</div>
+                  </Link>
+                ))}
+              </div>
+              <ModeToggle />
+            </div>
+            <HamburgerMenu navigations={navigationData.navigation?.link} />
+          </div>
         </div>
-      </div>
+      </header>
     </div>
   );
 };
