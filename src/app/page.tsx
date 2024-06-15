0042895b57ce2c52d97b5df1b9da55eb2479b8d4
LSzpilowski <lszpilowski@gmail.com>
2024-06-15 10:57:49 +0100

    add grid to post/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { PostsClient } from "./posts/PostClient";
+import { PostsGetListDocument } from "@/gql/graphql";
+import { executeGraphql } from "@/lib/graphql";
+
+export default async function Home() {
+  const { posts } = await executeGraphql({
+    query: PostsGetListDocument,
+  });
+
   return (
-    <div className="flex min-h-[calc(100vh-7rem)] flex-wrap items-center justify-center gap-5 p-5">
-      Home
-    </div>
+  <div className="min-h-[calc(100vh-6.3rem)]">
+  <PostsClient posts={posts} />
+  </div>
   );
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,23 @@
-import { PostsClient } from "./PostClient";
+import { PostCard } from "@/components/post-card";
 import { PostsGetListDocument } from "@/gql/graphql";
 import { executeGraphql } from "@/lib/graphql";
+
 
 export default async function Posts() {
   const { posts } = await executeGraphql({
     query: PostsGetListDocument,
   });
 
-  return <PostsClient posts={posts} />;
+  return (
+    <div className="flex min-h-[calc(100vh-6.3rem)] w-full flex-col items-center">
+        <h1 className=" text-4xl my-20">All posts</h1>
+        <div className="flex flex-row justify-center w-full">
+          <ul className="grid grid-cols-4 gap-5 w-11/12 px-10 mb-10" >
+            {posts.map((post, index) => (
+              <PostCard post={post} key={index} />
+            ))}
+        </ul>
+        </div>
+    </div>
+  );
 }
